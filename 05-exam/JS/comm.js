bdb2eee01d6b3708e3779cfd239c150f22a7f2e6
yunheeAhn <ayh95944674@gmail.com>
2023-12-15 15:25:35 +0900

    swiper사용법

---
+++ 05-exam/JS/comm.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+//   스와이퍼 연습
