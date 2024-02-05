cce43c55eed474b1cf37c673dec835b064f8d2bd
yunheeAhn <ayh95944674@gmail.com>
2024-02-05 12:28:47 +0900

    240205_Container.jsx 수정

--- 11-Dashboard-reactPackage/src/components/layout/Container.jsx
+++ 11-Dashboard-reactPackage/src/components/layout/Container.jsx
@@ -5,7 +5,7 @@ import { Box, Container } from "@chakra-ui/react";
 const Containerwrap = (props) => {
     return (
         <>
-            <Container display="flex" justifyContent={"space-between"} maxW={1280} mx={'auto'} px={['20px',null,0]} >
+            <Container maxW={1280} mx={'auto'} px={['20px',null,0]} >
                 {props.children}
             </Container>
         </>
@@ -13,8 +13,15 @@ const Containerwrap = (props) => {
 }
 
 export const Section = (props) => {
-    return <Box py={4}>{props.children}</Box>
-}
+    // 삼항 연산자
+    // 조건 ? 참일 때 : 거짓일 때
+    const backgroundColor = props.isLightBackground ? 'white' : 'gray.100'
+    return ( 
+        <Box py={'100px'} bg={backgroundColor}>
+            {props.children}
+        </Box>
+        )
+    }
 
 export default Containerwrap;
 
