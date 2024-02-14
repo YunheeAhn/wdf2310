0d1e430cf68d7a091857ea89172b476f14917ea9
yunheeAhn <ayh95944674@gmail.com>
2024-02-14 12:23:04 +0900

    240214_메인페이지 클론코딩

--- 11-Dashboard-reactPackage - 복사본/src/views/home/index.jsx
+++ 11-Dashboard-reactPackage - 복사본/src/views/home/index.jsx
@@ -1,11 +1,48 @@
 // 첫페이지
 
+import { Box, SimpleGrid, VStack } from "@chakra-ui/react";
+import Ministatics from "../../components/card/Ministatics";
+
 
 
 const Home = () => {
     return (
-        <>  
+        <>
+        
+        <VStack spacing="20px" width={'100%'}>
+
+            <SimpleGrid columns={{base : 1,md : 2,xl : 6}} spacing={10} width={'100%'}>
+                <Ministatics />
+                <Ministatics />
+                <Ministatics />
+                <Ministatics />
+                <Ministatics />
+                <Ministatics />
+            </SimpleGrid>
             
+            <SimpleGrid columns={{base : 1,md : 2}} spacing={10} width={'100%'}>
+                <Box bg={'tomato'} height={'90px'}></Box>
+                <Box bg={'tomato'} height={'90px'}></Box>
+            </SimpleGrid>
+
+            <SimpleGrid columns={{base : 1,md : 2}} spacing={10} width={'100%'}>
+                <Box bg={'tomato'} height={'90px'}></Box>
+                <SimpleGrid columns={{base : 1,md : 2}} spacing={10} width={'100%'}>
+                    <Box bg={'tomato'} height={'90px'}></Box>
+                <Box bg={'tomato'} height={'90px'}></Box>
+                </SimpleGrid>
+            </SimpleGrid>
+
+            <SimpleGrid columns={{base : 1,md : 2}} spacing={10} width={'100%'}>
+                <Box bg={'tomato'} height={'90px'}></Box>
+                <SimpleGrid columns={{base : 1,md : 2}} spacing={10} width={'100%'}>
+                    <Box bg={'tomato'} height={'90px'}></Box>
+                <Box bg={'tomato'} height={'90px'}></Box>
+                </SimpleGrid>
+            </SimpleGrid>
+
+        </VStack>
+
         </>
     )
 }
