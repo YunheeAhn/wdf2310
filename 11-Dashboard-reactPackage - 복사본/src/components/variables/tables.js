819a73cee576221b6ae43ffbe874f682fca9423a
yunheeAhn <ayh95944674@gmail.com>
2024-02-15 13:18:06 +0900

    240215_체크테이블컴포넌트 데이터 따로 관리하기

---
+++ 11-Dashboard-reactPackage - 복사본/src/components/variables/tables.js
@@ -0,0 +1,37 @@
+export const tabledataCheck = [
+    {
+        checked: false,
+        name: 'Marketplace',
+        progress: '76.5%',
+        quantity: '2458',
+        date: 'Apr 26, 2022'
+    },
+    {
+        checked: true,
+        name: 'Venus DB PRO',
+        progress: '35.4%',
+        quantity: '1458',
+        date: 'Jul 20, 2022'
+    },
+    {
+        checked: false,
+        name: 'Venus DS',
+        progress: '35.4%',
+        quantity: '1458',
+        date: 'Sep 30, 2022'
+    },
+    {
+        checked: false,
+        name: 'Venus 3D Asset',
+        progress: '100%',
+        quantity: '858',
+        date: 'Oct 24, 2022'
+    },
+    {
+        checked: true,
+        name: 'Marketplace',
+        progress: '76.5%',
+        quantity: '2458',
+        date: 'Apr 26, 2022'
+    }
+]
