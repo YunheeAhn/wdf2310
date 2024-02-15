d5e38ed50fa98cc49f63d9fdf50f28099c96db61
yunheeAhn <ayh95944674@gmail.com>
2024-02-15 13:19:07 +0900

    240215_차크라유아이 라인차트 삽입

--- 11-Dashboard-reactPackage - 복사본/src/views/home/components/WeeklyRevenue.jsx
+++ 11-Dashboard-reactPackage - 복사본/src/views/home/components/WeeklyRevenue.jsx
@@ -1,23 +1,28 @@
-import { Card, CardBody, Flex,Stat,StatLabel} from "@chakra-ui/react"
+import { Card, CardBody, CardHeader, Flex,Stat,StatLabel} from "@chakra-ui/react"
 import { faChartSimple} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import LineChart from '../../../components/charts/LineChart'
-import { lineChartDataTotalSpent, lineChartOptionsTotalSpent } from "../../../components/variables/charts";
+// import { barChartDataDailyTraffic, barChartOptionsDailyTraffic } from "../../../components/variables/charts";
+// import ColumnChart from "../../../components/charts/BarChart";
 
+import LineChart from "../../../components/charts/LineChart";
+import { lineChartDataTotalSpent, lineChartOptionsTotalSpent } from "../../../components/variables/charts";
 
 
 const WeeklyRevenue = ( ) => {
     return (
         <Card ml={'10px'}>
-            <Stat>
-                <Flex display={'flex'}  alignItems={'center'} justifyContent={'space-between'} borderRadius={'50%'} >
-                    <StatLabel color={'brand.200'}>This month</StatLabel>
-                        <FontAwesomeIcon icon={faChartSimple} color='#422AFB' />
-                </Flex>
-                <CardBody>
-                    <LineChart chartData={lineChartDataTotalSpent} chartOptions={lineChartOptionsTotalSpent} />
-                </CardBody>
-            </Stat>
+            <CardHeader>
+                <Stat>
+                    <Flex display={'flex'}  alignItems={'center'} justifyContent={'space-between'} borderRadius={'50%'} >
+                        <StatLabel color={'brand.200'}>This month</StatLabel>
+                            <FontAwesomeIcon icon={faChartSimple} color='#422AFB' />
+                    </Flex>
+                </Stat>    
+            </CardHeader>
+            
+                <LineChart chartData={lineChartDataTotalSpent} chartOptions={lineChartOptionsTotalSpent} />
+                
+            
         </Card>
     )
 }
