d485c5f35812b58b5c164708dcdead595117e076
YunheeAhn <ayh95944674@gmail.com>
2024-02-19 14:33:01 +0900

    240219_마켓플레이스페이지 화면 구성

--- 11-Dashboard-reactPackage - 복사본/src/views/marketplace/Marketplace.jsx
+++ 11-Dashboard-reactPackage - 복사본/src/views/marketplace/Marketplace.jsx
@@ -1,37 +1,29 @@
-// 마켓플레이스 페이지
+// 마켓플레이스 페이지 클론 코딩
 
-import { Box, Img } from "@chakra-ui/react";
-// import Layout from "../../components/layout/Layout";
-import { motion } from 'framer-motion';
-import topImg from '../../assets/images/31448559_about_media_38ef19cd-33e8-4842-aa7c-2ca5473ae3a3.avif'
+import { Box, Flex, Grid, GridItem } from "@chakra-ui/react";
+import Banner from "./components/Banner";
+
 
 
 const Marketplace = () => {
-    const boxStyle = {
-        width : '200px',
-        height : '200px',
-        background : 'lightpink',
-        borderRadius : '50%'
-    }
-
+    
     return (
         < >
-            <Box className="top-cont" h={'500px'} bg={`url(${topImg}) no-repeat 50% 50% / cover`}>
-                {/* 리액트의 경우 이미지를 src폴더에 저장 하면, import해서 변수처럼 설정 후 데이터값으로 가져온다 */}
-            </Box>
-            <Img src={topImg} alt="/" top={0} />
-            <Img src="/logo192.png" alt="/" />
-            {/* public 폴더 안 이미지는 src="/파일명"으로 불러온다. 단, 지양하자 */}
-            {/* <div>마켓플레이스 컨텐츠</div> */}
-
-            
-
-            <motion.div style = {boxStyle} initial={{ opacity: 0 }} animate={{ opacity: 1, x: 100 }} transition={{ ease: 'easeOut', duration: 2 }} />
-            <motion.div style = {boxStyle} initial={{ opacity: 0 }} animate={{ opacity: 1, x: 100 }} transition={{ ease: 'easeOut', duration: 2 }} />
-            <motion.div style = {boxStyle} initial={{ opacity: 0 }} animate={{ opacity: 1, x: 100 }} transition={{ ease: 'easeOut', duration: 2 }} />
-            <motion.div style = {boxStyle} initial={{ opacity: 0 }} animate={{ opacity: 1, x: 100 }} transition={{ ease: 'easeOut', duration: 2 }} />
-            <motion.div style = {boxStyle} initial={{ opacity: 0 }} animate={{ opacity: 1, x: 100 }} transition={{ ease: 'easeOut', duration: 2 }} />
-            
+            <Grid p={'10px'} templateColumns='repeat(3, 1fr)' gap={'10px'} display={{base: 'block', xl : 'grid' }}>
+                <GridItem gridColumn={'1 / span 2'} w='100%' h='10'  >
+                    <Flex flexDir={'column'} gap={'10px'}>
+                        <Banner w={'100%'} bg={'tomato'} height={'90px'}></Banner>
+                        <Box w={'100%'} bg={'tomato'} height={'90px'}></Box>
+                        <Box w={'100%'} bg={'tomato'} height={'90px'}></Box>
+                    </Flex>
+                </GridItem>
+                <GridItem w='100%' h='10' >
+                    <Flex flexDir={'column'} gap={'10px'}>
+                        <Box w={'100%'} bg={'tomato'} height={'90px'}></Box>
+                        <Box w={'100%'} bg={'tomato'} height={'90px'}></Box>
+                    </Flex>
+                </GridItem>
+            </Grid>
         </>    
     )
 }
