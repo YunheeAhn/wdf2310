6e3d7db86f3bd6c680d81e84e8c166e6ae3d3aaa
YunheeAhn <ayh95944674@gmail.com>
2024-03-11 15:32:54 +0900

    240311_표준내장객체

---
+++ 07-JS/JS/표준내장객체.js
@@ -0,0 +1,13 @@
+// 표준 내장 객체
+console.log('표준내장객체 Date')
+console.log('1. Date객체')
+console.log(`new Date() -> ${new Date()}`)
+console.log(`new Date().getFullYear() -> ${new Date().getFullYear()}`)
+console.log(`new Date().getMonth() -> ${new Date().getMonth()}`)
+console.log(`new Date().getDate() -> ${new Date().getDate()}`)
+console.log(`new Date().getDay() -> ${new Date().getDay()}`)
+
+console.log(`new Date().getHours() -> ${new Date().getHours()}`)
+console.log(`new Date().getMinutes() -> ${new Date().getMinutes()}`)
+console.log(`new Date().getSeconds() -> ${new Date().getSeconds()}`)
+console.log(`new Date().getMilliseconds() -> ${new Date().getMilliseconds()}`)
